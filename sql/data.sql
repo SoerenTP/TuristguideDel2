2b20e31b75d4b1d3d4b9ab6d594e104ff7f02032
SoerenTP <sope0001@stud.kea.dk>
2024-03-20 09:03:52 +0100

    Connection

---
+++ sql/data.sql
@@ -0,0 +1,8 @@
+# tags
+INSERT INTO tag (name) VALUES("Museum");
+INSERT INTO tag (name) VALUES("Free");
+INSERT INTO tag (name) VALUES("Paid");
+INSERT INTO tag (name) VALUES("Family Friendly");
+INSERT INTO tag (name) VALUES("Historical");
+INSERT INTO tag (name) VALUES("Religion");
+INSERT INTO tag (name) VALUES("Amusement Park");
---
+++ sql/schema.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS turist_guide;
+USE turist_guide;
+
+CREATE TABLE attraction (
+    attraction_id INT AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    PRIMARY KEY (attraction_id),
+);
+
+CREATE TABLE tag (
+    tag_id INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (tag_id)
+);
+
+CREATE TABLE attraction_tags (
+    attraction_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY (attraction_id) REFERENCES attraction(attraction_id),
+    FOREIGN KEY (tag_id) REFERENCES tag(tag_id)
+)
